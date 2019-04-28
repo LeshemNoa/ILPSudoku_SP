304f2a0088f1fada3f98fcd216f1b4e543fbeb74
Noa Leshem <>
2019-04-28 22:38:03 +0300

    More CR response: improved BT_solver algorithm runtime verified no empty moves are pushed on the undo redo list

--- BT_solver.c
+++ BT_solver.c
@@ -8,14 +8,23 @@ typedef struct {
     int curCol;
 } CallFrame;
 
-bool pushCallFrame(Stack* stack, int curRow, int curCol) {
+typedef struct BacktrackSolverState {
+	int MN;	
+	Board puzzle;
+	Stack callStack;
+	int** rowsCellsValuesCounters;
+	int** columnsCellsValuesCounters;
+	int** blocksCellsValuesCounters;
+} BacktrackSolverState;
+
+bool pushCallFrame(BacktrackSolverState* state, int curRow, int curCol) {
     CallFrame* frame = (CallFrame*)calloc(1, sizeof(CallFrame));
     if (frame == NULL) {
         return false;
     }
     frame->curRow = curRow;
     frame->curCol = curCol;
-    if (!pushStack(stack, frame)) {
+    if (!pushStack(&(state->callStack), frame)) {
         free(frame);
         return false;
     }
@@ -23,20 +32,20 @@ bool pushCallFrame(Stack* stack, int curRow, int curCol) {
     return true;
 }
 
-bool peekCallFrame(const Stack* stack, int* curRow, int* curCol) {
+bool peekCallFrame(BacktrackSolverState* state, int* curRow, int* curCol) {
     const CallFrame* frame;
-    if (isStackEmpty(stack)) {
+    if (isStackEmpty(&(state->callStack))) {
         return false;
     }
     
-    frame = (const CallFrame*)peekStack(stack);
+    frame = (const CallFrame*)peekStack(&(state->callStack));
     *curRow = frame->curRow;
     *curCol = frame->curCol;
     return true;
 }
 
-bool popCallFrame(Stack* stack) {
-    CallFrame* frame = (CallFrame*) popStack(stack);
+bool popCallFrame(BacktrackSolverState* state) {
+    CallFrame* frame = (CallFrame*) popStack(&(state->callStack));
     if (frame == NULL) {
         return false;
     }
@@ -45,79 +54,168 @@ bool popCallFrame(Stack* stack) {
     return true;
 }
 
-void clearCallFrames(Stack* stack) {
-    while(!isStackEmpty(stack)) {
-        popCallFrame(stack);
+void clearCallFrames(BacktrackSolverState* state) {
+    while(!isStackEmpty(&(state->callStack))) {
+        popCallFrame(state);
     }
 }
+
+void freeSolverCellsValuesCounters(BacktrackSolverState* state) {
+	if (state->rowsCellsValuesCounters != NULL) {
+		freeSpecificCellsValuesCounters(state->rowsCellsValuesCounters, &(state->puzzle));
+		state->rowsCellsValuesCounters = NULL;
+	}
+	if (state->columnsCellsValuesCounters != NULL) {
+		freeSpecificCellsValuesCounters(state->columnsCellsValuesCounters, &(state->puzzle));
+		state->columnsCellsValuesCounters = NULL;
+	}
+	if (state->blocksCellsValuesCounters != NULL) {
+		freeSpecificCellsValuesCounters(state->blocksCellsValuesCounters, &(state->puzzle));
+		state->blocksCellsValuesCounters = NULL;
+	}
+}
+
+bool createSolverCellsValuesCounters(BacktrackSolverState* state) {
+	state->rowsCellsValuesCounters = createCellsValuesCountersByCategory(&(state->puzzle), viewBoardCellByRow);
+	state->columnsCellsValuesCounters = createCellsValuesCountersByCategory(&(state->puzzle), viewBoardCellByColumn);
+	state->blocksCellsValuesCounters = createCellsValuesCountersByCategory(&(state->puzzle), viewBoardCellByBlock);
+
+	if ((state->rowsCellsValuesCounters != NULL) &&
+		(state->columnsCellsValuesCounters != NULL) &&
+		(state->blocksCellsValuesCounters != NULL)) {
+		return true;
+	}
+
+	freeSolverCellsValuesCounters(state);
+	return false;
+}
+
+bool initSolverState(BacktrackSolverState* state, const Board* boardIn) {
+	state->rowsCellsValuesCounters = NULL;
+	state->columnsCellsValuesCounters = NULL;
+	state->blocksCellsValuesCounters = NULL;
+	state->MN = getBoardBlockSize_MN(boardIn);
+
+	initStack(&state->callStack);
+	if (!copyBoard(boardIn, &(state->puzzle))) {
+		/* memory error */
+		return false;
+	}
+	
+	if (!createSolverCellsValuesCounters(state)) {
+		/* memory error */
+		cleanupBoard(&(state->puzzle));
+		return false;
+	}
+
+	return true;
+}
+
+void cleanupSolverState(BacktrackSolverState* state) {
+	clearCallFrames(state);
+	cleanupBoard(&(state->puzzle));
+	freeSolverCellsValuesCounters(state);
+}
+
+bool isValueLegalForSolverCell(BacktrackSolverState* state, int row, int col, int value) {
+	int block = whichBlock(&(state->puzzle), row, col);
+	return (state->rowsCellsValuesCounters[row][value] == 0) &&
+		   (state->columnsCellsValuesCounters[col][value] == 0) &&
+		   (state->blocksCellsValuesCounters[block][value] == 0);
+}
+
+/* Maintaining the invariant: at all times, all cells' value counters are updated
+correctly after each change in the board. Returning the previous value
+of that cell */
+int setSolverPuzzleCell(BacktrackSolverState* state, int row, int col, int value) {
+	Cell* cell = getBoardCellByRow(&(state->puzzle), row, col);
+	int block, prevValue = getBoardCellValue(cell);
+	if (prevValue == value) {
+		/* Nothing to be done */
+		return value;
+	}
+
+	block = whichBlock(&(state->puzzle), row, col);
+
+	if (prevValue != EMPTY_CELL_VALUE) {
+		state->rowsCellsValuesCounters[row][prevValue]--;
+		state->columnsCellsValuesCounters[col][prevValue]--;
+		state->blocksCellsValuesCounters[block][prevValue]--;
+	}
+
+	if (value != EMPTY_CELL_VALUE) {
+		state->rowsCellsValuesCounters[row][value]++;
+		state->columnsCellsValuesCounters[col][value]++;
+		state->blocksCellsValuesCounters[block][value]++;
+	}
+
+	setBoardCellValue(&(state->puzzle), row, col, value);
+	return prevValue;
+}
+
 /* pre-condition: the input board is not erroneous. */
 bool calculateNumSolutions(const Board* boardIn, int* numSolutions) {
-	Stack stack = {0};
+	BacktrackSolverState state = {0};
+
 	int curCol, curRow;
 	int sum = 0;
-	int MN = getBoardBlockSize_MN(boardIn);
-	Board boardCopy = {0};
 	
 	if (!getNextEmptyBoardCell(boardIn, 0, 0, &curRow, &curCol)) {
 		*numSolutions = 1; /* board is full and has no errors (pre-condition) */
 		return true;
 	}
 
-	if (!copyBoard(boardIn, &boardCopy)) {
-		/* memory error */
+	if (!initSolverState(&state, boardIn)) {
 		return false;
 	}
-
-	initStack(&stack);
 
 	/* push initial empty cell */
-	if (!pushCallFrame(&stack, curRow, curCol)) {
+	if (!pushCallFrame(&state, curRow, curCol)) {
 		/* memory error */
-		cleanupBoard(&boardCopy);
+		cleanupSolverState(&state);
 		return false;
 	}
 
-	while (peekCallFrame(&stack, &curRow, &curCol)) {
-		Cell* cell;
+	while (peekCallFrame(&state, &curRow, &curCol)) {
 		int nextRow, nextCol, newValue;
 		bool isLegalValue;
+		const Cell* cell = viewBoardCellByRow(&(state.puzzle), curRow, curCol);
 
-		cell = getBoardCellByRow(&boardCopy, curRow, curCol);
-		if (getBoardCellValue(cell) == MN) { /* max value */
+		/* increment value, not assuming EMPTY_CELL_VALUE == 0 */
+		newValue = isBoardCellEmpty(cell) ? 1 : getBoardCellValue(cell) + 1;
+
+		if (newValue == state.MN + 1) { /* max value */
 			/* back track */
-			emptyBoardCell(cell);
-			popCallFrame(&stack);
+			setSolverPuzzleCell(&state, curRow, curCol, EMPTY_CELL_VALUE);
+			popCallFrame(&state);
 			continue;
 		}
 
-		/* increment value, not assuming EMPTY_CELL_VALUE == 0 */
-		newValue = isBoardCellEmpty(cell) ? 1 : cell->value + 1;
 		/* check if board is valid after incrementing value*/
-		isLegalValue = isValueLegalForBoardCell(&boardCopy, curRow, curCol, newValue); /* CR: note that calling this function will result in the same running time as the previous code did (which updated erroneousness). Surely you must have noticed that if you ran num_solutions on a large-enough board. You could save running time by creating counters. I leave it to you, to consider doing this or not (i.e. mimicking what isValueLegalForCell does in game module */
+		isLegalValue = isValueLegalForSolverCell(&state, curRow, curCol, newValue);
 		/* set value anyway (after checking legality) */
-		setBoardCellValue(&boardCopy, curRow, curCol, newValue);
-		
+		setSolverPuzzleCell(&state, curRow, curCol, newValue);
+
 		if (!isLegalValue) {
 			/* illegal, try next value */
 			continue;
 		}
 
-		if (!getNextEmptyBoardCell(&boardCopy, curRow, curCol, &nextRow, &nextCol)) {
+		if (!getNextEmptyBoardCell(&(state.puzzle), curRow, curCol, &nextRow, &nextCol)) {
 			/* no more empty cells, count as solved */
 			sum++;
 			continue;
 		}
 
 		/* count solutions for next empty cell given current board */
-		if (!pushCallFrame(&stack, nextRow, nextCol)) {
+		if (!pushCallFrame(&state, nextRow, nextCol)) {
 			/* memory error */
-			clearCallFrames(&stack);
-			cleanupBoard(&boardCopy);
+			cleanupSolverState(&state);
 			return false;
 		}
 	}
 
-	cleanupBoard(&boardCopy);
+	cleanupSolverState(&state);
 	*numSolutions = sum;
 	return true;
 }
